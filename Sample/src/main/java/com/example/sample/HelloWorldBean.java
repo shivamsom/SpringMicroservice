b54e23b3513ee53083d2081ef5c08ec0d6580c75
shivamsom <shivamsom3@gmail.com>
2018-08-08 16:11:34 +0530

    Sample Spring Hello World

---
+++ Sample/src/main/java/com/example/sample/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.example.sample;
+
+public class HelloWorldBean {
+
+	private String msg;
+    
+	
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	
+}
